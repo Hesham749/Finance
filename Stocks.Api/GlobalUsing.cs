acc885cc695b82514611ee43c884d10d552c37b7
Hesham Elsayed <heesham749@gmail.com>
2025-03-17 11:37:24 +0200

    Add Comment and Stock models with data annotations
    Introduces `Comment` and `Stock` classes in the `Stocks.Api.Models` namespace.
    The `Comment` class includes properties for `Id`, `Title`, `Content`, `CreatedOn`,
    `StockId`, and a navigation property to `Stock`. The `Stock` class features properties
    for `Id`, `Symbol`, `CompanyName`, `Purchase`, `LastDiv`, `Industry`, `MarketCap`,
    and a collection of `Comments`. Updates to `GlobalUsing.cs` include new using
    directives for data annotations.

--- Stocks.Api/GlobalUsing.cs
+++ Stocks.Api/GlobalUsing.cs
@@ -1,4 +1,6 @@
 ﻿global using Microsoft.AspNetCore.Identity;
 global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+global using System.ComponentModel.DataAnnotations.Schema;
 global using Microsoft.EntityFrameworkCore;
+global using System.ComponentModel.DataAnnotations;
 
---
+++ Stocks.Api/Models/Comment.cs
@@ -0,0 +1,20 @@
+﻿namespace Stocks.Api.Models
+{
+    [Table("Comments")]
+    public class Comment
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(255)]
+        public string Title { get; set; }
+
+        [Required, MaxLength(255)]
+        public string Content { get; set; }
+
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+
+        public int? StockId { get; set; }
+
+        public Stock Stock { get; set; }
+    }
+}
